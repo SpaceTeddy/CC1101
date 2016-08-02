d6b3e2e1440b95dc171654287eff5023dc7479d1
SpaceTeddy <spaceteddy1@gmx.net>
2016-08-02 14:57:49 +0200

    Update eeprom_create.py

--- Tools/eeprom_create.py
+++ Tools/eeprom_create.py
@@ -80,6 +80,9 @@ if(max_lenght == 32):
 print (txt_neu)
 f.write("EEPROM_LEN = " + str(total_bytes) + "\n")
 f.write("uint8_t eeprom_cc1101[EEPROM_LEN] = { \n")
-f.write("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F" + "\n")
+if(max_lenght == 16):
+    f.write("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F + "\n"")
+if(max_lenght == 32):
+    f.write("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F   10   11   12   13   14   15   16   17   18   19   1A   1B   1C   1D   1E   1F" + "\n")
 f.write(txt_neu)
 f.close()
