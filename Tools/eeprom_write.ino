d10fb7053dcf8a11de7742ed1cbd7131928f9e26
SpaceTeddy <spaceteddy1@gmx.net>
2016-08-03 07:43:28 +0200

    Update eeprom_write.ino

--- Tools/eeprom_write.ino
+++ Tools/eeprom_write.ino
@@ -22,7 +22,7 @@ int value;
 
 //-----------------------------------------------------------------------------
 
-//------------------[ paste date from array python tool here ]-----------------
+//------------------[ paste data from array python tool here ]-----------------
 #define EEPROM_LEN 313                                              //array len
 uint8_t eeprom_cc1101[EEPROM_LEN] = {
 //00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F
