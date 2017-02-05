24e432f4339885916920320a261925f05e2dbfc5
SpaceTeddy <spaceteddy1@gmx.net>
2017-02-05 10:52:54 +0100

    Update cc1100_raspi.cpp
    added register settings for 1.2kbaud OOK support

--- cc1100_raspi.cpp
+++ cc1100_raspi.cpp
@@ -20,41 +20,260 @@
 CC1100 cc1100;
 
 extern uint8_t cc1100_debug;
-//-------------------[global default settings 868 Mhz]---------------------------------
 
-static uint8_t cc1100_GFSK_1_2_kb[CFG_REGISTER] = {0x07,0x2E,0x80,0x07,0x57,0x43,0x3E,0x0D,0x45,0xFF,
-										0x00,0x08,0x00,0x21,0x65,0x6A,0xF5,0x83,0x13,0xA0,
-										0xF8,0x15,0x07,0x0C,0x19,0x16,0x6C,0x03,0x40,0x91,
-										0x02,0x26,0x09,0x56,0x17,0xA9,0x0A,0x00,0x11,0x41,
-										0x00,0x59,0x7F,0x3F,0x81,0x3F,0x0B};
+//-------------------[global EEPROM default settings 868 Mhz]-------------------
+static uint8_t cc1100_GFSK_1_2_kb[CFG_REGISTER] = {
+					0x07,  // IOCFG2        GDO2 Output Pin Configuration
+					0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+				  	0x80,  // IOCFG0        GDO0 Output Pin Configuration
+					0x07,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
+				  	0x57,  // SYNC1         Sync Word, High Byte
+					0x43,  // SYNC0         Sync Word, Low Byte
+					0x3E,  // PKTLEN        Packet Length
+					0x0E,  // PKTCTRL1      Packet Automation Control
+					0x45,  // PKTCTRL0      Packet Automation Control
+					0xFF,  // ADDR          Device Address
+					0x00,  // CHANNR        Channel Number
+					0x08,  // FSCTRL1       Frequency Synthesizer Control
+					0x00,  // FSCTRL0       Frequency Synthesizer Control
+					0x21,  // FREQ2         Frequency Control Word, High Byte
+					0x65,  // FREQ1         Frequency Control Word, Middle Byte
+					0x6A,  // FREQ0         Frequency Control Word, Low Byte
+					0xF5,  // MDMCFG4       Modem Configuration
+					0x83,  // MDMCFG3       Modem Configuration
+					0x13,  // MDMCFG2       Modem Configuration
+					0xA0,  // MDMCFG1       Modem Configuration
+					0xF8,  // MDMCFG0       Modem Configuration
+				  	0x15,  // DEVIATN       Modem Deviation Setting
+					0x07,  // MCSM2         Main Radio Control State Machine Configuration
+					0x0C,  // MCSM1         Main Radio Control State Machine Configuration
+					0x19,  // MCSM0         Main Radio Control State Machine Configuration
+					0x16,  // FOCCFG        Frequency Offset Compensation Configuration
+					0x6C,  // BSCFG         Bit Synchronization Configuration
+					0x03,  // AGCCTRL2      AGC Control
+				  	0x40,  // AGCCTRL1      AGC Control
+					0x91,  // AGCCTRL0      AGC Control
+					0x02,  // WOREVT1       High Byte Event0 Timeout
+					0x26,  // WOREVT0       Low Byte Event0 Timeout
+					0x09,  // WORCTRL       Wake On Radio Control
+					0x56,  // FREND1        Front End RX Configuration
+				  	0x17,  // FREND0        Front End TX Configuration
+					0xA9,  // FSCAL3        Frequency Synthesizer Calibration
+					0x0A,  // FSCAL2        Frequency Synthesizer Calibration
+					0x00,  // FSCAL1        Frequency Synthesizer Calibration
+				  	0x11,  // FSCAL0        Frequency Synthesizer Calibration
+					0x41,  // RCCTRL1       RC Oscillator Configuration
+					0x00,  // RCCTRL0       RC Oscillator Configuration
+				  	0x59,  // FSTEST        Frequency Synthesizer Calibration Control,
+					0x7F,  // PTEST         Production Test
+					0x3F,  // AGCTEST       AGC Test
+					0x81,  // TEST2         Various Test Settings
+					0x3F,  // TEST1         Various Test Settings
+					0x0B   // TEST0         Various Test Settings
+				};
 
-static uint8_t cc1100_GFSK_38_4_kb[CFG_REGISTER] = {0x07,0x2E,0x80,0x07,0x57,0x43,0x3E,0x0E,0x45,0xFF,
-										0x00,0x06,0x00,0x21,0x65,0x6A,0xCA,0x83,0x13,0xA0,
-										0xF8,0x34,0x07,0x0C,0x19,0x16,0x6C,0x43,0x40,0x91,
-										0x02,0x26,0x09,0x56,0x17,0xA9,0x0A,0x00,0x11,0x41,
-										0x00,0x59,0x7F,0x3F,0x81,0x3F,0x0B};
+static uint8_t cc1100_GFSK_38_4_kb[CFG_REGISTER] = {
+					0x07,  // IOCFG2        GDO2 Output Pin Configuration
+					0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+				  	0x80,  // IOCFG0        GDO0 Output Pin Configuration
+					0x07,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
+					0x57,  // SYNC1         Sync Word, High Byte
+					0x43,  // SYNC0         Sync Word, Low Byte
+				  	0x3E,  // PKTLEN        Packet Length
+					0x0E,  // PKTCTRL1      Packet Automation Control
+					0x45,  // PKTCTRL0      Packet Automation Control
+					0xFF,  // ADDR          Device Address
+					0x00,  // CHANNR        Channel Number
+					0x06,  // FSCTRL1       Frequency Synthesizer Control
+				  	0x00,  // FSCTRL0       Frequency Synthesizer Control
+					0x21,  // FREQ2         Frequency Control Word, High Byte
+					0x65,  // FREQ1         Frequency Control Word, Middle Byte
+			   		0x6A,  // FREQ0         Frequency Control Word, Low Byte
+				  	0xCA,  // MDMCFG4       Modem Configuration
+					0x83,  // MDMCFG3       Modem Configuration
+			  		0x13,  // MDMCFG2       Modem Configuration
+					0xA0,  // MDMCFG1       Modem Configuration
+					0xF8,  // MDMCFG0       Modem Configuration
+					0x34,  // DEVIATN       Modem Deviation Setting
+					0x07,  // MCSM2         Main Radio Control State Machine Configuration
+					0x0C,  // MCSM1         Main Radio Control State Machine Configuration
+				  	0x19,  // MCSM0         Main Radio Control State Machine Configuration
+					0x16,  // FOCCFG        Frequency Offset Compensation Configuration
+					0x6C,  // BSCFG         Bit Synchronization Configuration
+					0x43,  // AGCCTRL2      AGC Control
+					0x40,  // AGCCTRL1      AGC Control
+				  	0x91,  // AGCCTRL0      AGC Control
+					0x02,  // WOREVT1       High Byte Event0 Timeout
+					0x26,  // WOREVT0       Low Byte Event0 Timeout
+					0x09,  // WORCTRL       Wake On Radio Control
+					0x56,  // FREND1        Front End RX Configuration
+					0x17,  // FREND0        Front End TX Configuration
+					0xA9,  // FSCAL3        Frequency Synthesizer Calibration
+					0x0A,  // FSCAL2        Frequency Synthesizer Calibration
+					0x00,  // FSCAL1        Frequency Synthesizer Calibration
+					0x11,  // FSCAL0        Frequency Synthesizer Calibration
+					0x41,  // RCCTRL1       RC Oscillator Configuration
+			  		0x00,  // RCCTRL0       RC Oscillator Configuration
+					0x59,  // FSTEST        Frequency Synthesizer Calibration Control,
+					0x7F,  // PTEST         Production Test
+					0x3F,  // AGCTEST       AGC Test
+					0x81,  // TEST2         Various Test Settings
+					0x3F,  // TEST1         Various Test Settings
+					0x0B   // TEST0         Various Test Settings
+				};
 
-static uint8_t cc1100_GFSK_100_kb[CFG_REGISTER] = {0x07,0x2E,0x80,0x07,0x57,0x43,0x3E,0x0E,0x45,0xFF,
-									  0x00,0x08,0x00,0x21,0x65,0x6A,0x5B,0xF8,0x13,0xA0,
-									  0xF8,0x47,0x07,0x0C,0x18,0x1D,0x1C,0xC7,0x00,0xB2,
-									  0x02,0x26,0x09,0xB6,0x17,0xEA,0x0A,0x00,0x11,0x41,
-									  0x00,0x59,0x7F,0x3F,0x81,0x3F,0x0B};
+static uint8_t cc1100_GFSK_100_kb[CFG_REGISTER] = {
+					0x07,  // IOCFG2        GDO2 Output Pin Configuration
+					0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+					0x80,  // IOCFG0        GDO0 Output Pin Configuration
+					0x07,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
+					0x57,  // SYNC1         Sync Word, High Byte
+					0x43,  // SYNC0         Sync Word, Low Byte
+					0x3E,  // PKTLEN        Packet Length
+					0x0E,  // PKTCTRL1      Packet Automation Control
+					0x45,  // PKTCTRL0      Packet Automation Control
+					0xFF,  // ADDR          Device Address
+					0x00,  // CHANNR        Channel Number
+					0x08,  // FSCTRL1       Frequency Synthesizer Control
+					0x21,  // FREQ2         Frequency Control Word, High Byte
+					0x65,  // FREQ1         Frequency Control Word, Middle Byte
+					0x6A,  // FREQ0         Frequency Control Word, Low Byte
+					0x5B,  // MDMCFG4       Modem Configuration
+					0xF8,  // MDMCFG3       Modem Configuration
+					0x13,  // MDMCFG2       Modem Configuration
+					0xA0,  // MDMCFG1       Modem Configuration
+					0xF8,  // MDMCFG0       Modem Configuration
+					0x47,  // DEVIATN       Modem Deviation Setting
+				  	0x07,  // MCSM2         Main Radio Control State Machine Configuration
+					0x0C,  // MCSM1         Main Radio Control State Machine Configuration
+					0x18,  // MCSM0         Main Radio Control State Machine Configuration
+					0x1D,  // FOCCFG        Frequency Offset Compensation Configuration
+					0x1C,  // BSCFG         Bit Synchronization Configuration
+					0xC7,  // AGCCTRL2      AGC Control
+					0x00,  // AGCCTRL1      AGC Control
+					0xB2,  // AGCCTRL0      AGC Control
+					0x02,  // WOREVT1       High Byte Event0 Timeout
+					0x26,  // WOREVT0       Low Byte Event0 Timeout
+					0x09,  // WORCTRL       Wake On Radio Control
+					0xB6,  // FREND1        Front End RX Configuration
+					0x17,  // FREND0        Front End TX Configuration
+					0xEA,  // FSCAL3        Frequency Synthesizer Calibration
+					0x0A,  // FSCAL2        Frequency Synthesizer Calibration
+					0x00,  // FSCAL1        Frequency Synthesizer Calibration
+					0x11,  // FSCAL0        Frequency Synthesizer Calibration
+					0x41,  // RCCTRL1       RC Oscillator Configuration
+					0x00,  // RCCTRL0       RC Oscillator Configuration
+					0x59,  // FSTEST        Frequency Synthesizer Calibration Control,
+					0x7F,  // PTEST         Production Test
+				  	0x3F,  // AGCTEST       AGC Test
+				  	0x81,  // TEST2         Various Test Settings
+					0x3F,  // TEST1         Various Test Settings
+			   		0x0B   // TEST0         Various Test Settings
+				};
 
-static uint8_t cc1100_MSK_250_kb[CFG_REGISTER] = {0x07,0x2E,0x80,0x07,0x57,0x43,0x3E,0x0E,0x45,0xFF,
-									  0x00,0x0B,0x00,0x21,0x65,0x6A,0x2D,0x3B,0x73,0xA0,
-									  0xF8,0x00,0x07,0x0C,0x18,0x1D,0x1C,0xC7,0x00,0xB2,
-									  0x02,0x26,0x09,0xB6,0x17,0xEA,0x0A,0x00,0x11,0x41,
-									  0x00,0x59,0x7F,0x3F,0x81,0x3F,0x0B};
+static uint8_t cc1100_MSK_250_kb[CFG_REGISTER] = {
+					0x07,  // IOCFG2        GDO2 Output Pin Configuration
+				  	0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+					0x80,  // IOCFG0        GDO0 Output Pin Configuration
+					0x07,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
+					0x57,  // SYNC1         Sync Word, High Byte
+					0x43,  // SYNC0         Sync Word, Low Byte
+					0x3E,  // PKTLEN        Packet Length
+					0x0E,  // PKTCTRL1      Packet Automation Control
+					0x45,  // PKTCTRL0      Packet Automation Control
+				  	0xFF,  // ADDR          Device Address
+				  	0x00,  // CHANNR        Channel Number
+					0x0B,  // FSCTRL1       Frequency Synthesizer Control
+					0x00,  // FSCTRL0       Frequency Synthesizer Control
+					0x21,  // FREQ2         Frequency Control Word, High Byte
+					0x65,  // FREQ1         Frequency Control Word, Middle Byte
+					0x6A,  // FREQ0         Frequency Control Word, Low Byte
+					0x2D,  // MDMCFG4       Modem Configuration
+					0x3B,  // MDMCFG3       Modem Configuration
+				  	0x73,  // MDMCFG2       Modem Configuration
+					0xA0,  // MDMCFG1       Modem Configuration
+					0xF8,  // MDMCFG0       Modem Configuration
+					0x00,  // DEVIATN       Modem Deviation Setting
+				  	0x07,  // MCSM2         Main Radio Control State Machine Configuration
+					0x0C,  // MCSM1         Main Radio Control State Machine Configuration
+					0x18,  // MCSM0         Main Radio Control State Machine Configuration
+					0x1D,  // FOCCFG        Frequency Offset Compensation Configuration
+					0x1C,  // BSCFG         Bit Synchronization Configuration
+				  	0xC7,  // AGCCTRL2      AGC Control
+					0x00,  // AGCCTRL1      AGC Control
+					0xB2,  // AGCCTRL0      AGC Control
+					0x02,  // WOREVT1       High Byte Event0 Timeout
+					0x26,  // WOREVT0       Low Byte Event0 Timeout
+					0x09,  // WORCTRL       Wake On Radio Control
+				  	0xB6,  // FREND1        Front End RX Configuration
+					0x17,  // FREND0        Front End TX Configuration
+					0xEA,  // FSCAL3        Frequency Synthesizer Calibration
+					0x0A,  // FSCAL2        Frequency Synthesizer Calibration
+					0x00,  // FSCAL1        Frequency Synthesizer Calibration
+					0x11,  // FSCAL0        Frequency Synthesizer Calibration
+					0x41,  // RCCTRL1       RC Oscillator Configuration
+					0x00,  // RCCTRL0       RC Oscillator Configuration
+					0x59,  // FSTEST        Frequency Synthesizer Calibration Control,
+					0x7F,  // PTEST         Production Test
+				  	0x3F,  // AGCTEST       AGC Test
+					0x81,  // TEST2         Various Test Settings
+					0x3F,  // TEST1         Various Test Settings
+					0x0B   // TEST0         Various Test Settings
+				};
 
-static uint8_t cc1100_MSK_500_kb[CFG_REGISTER] = {0x07,0x2E,0x80,0x07,0x57,0x43,0x3E,0x0E,0x45,0xFF,
-									  0x00,0x0C,0x00,0x21,0x65,0x6A,0x0E,0x3B,0x73,0xA0,
-									  0xF8,0x00,0x07,0x0C,0x18,0x1D,0x1C,0xC7,0x40,0xB2,
-									  0x02,0x26,0x09,0xB6,0x17,0xEA,0x0A,0x00,0x19,0x41,
-									  0x00,0x59,0x7F,0x3F,0x81,0x3F,0x0B};
+static uint8_t cc1100_MSK_500_kb[CFG_REGISTER] = {
+					0x07,  // IOCFG2        GDO2 Output Pin Configuration
+					0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+					0x80,  // IOCFG0        GDO0 Output Pin Configuration
+					0x07,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
+					0x57,  // SYNC1         Sync Word, High Byte
+			    		0x43,  // SYNC0         Sync Word, Low Byte
+					0x3E,  // PKTLEN        Packet Length
+					0x0E,  // PKTCTRL1      Packet Automation Control
+					0x45,  // PKTCTRL0      Packet Automation Control
+					0xFF,  // ADDR          Device Address
+					0x00,  // CHANNR        Channel Number
+					0x0C,  // FSCTRL1       Frequency Synthesizer Control
+					0x00,  // FSCTRL0       Frequency Synthesizer Control
+					0x21,  // FREQ2         Frequency Control Word, High Byte
+					0x65,  // FREQ1         Frequency Control Word, Middle Byte
+					0x6A,  // FREQ0         Frequency Control Word, Low Byte
+					0x0E,  // MDMCFG4       Modem Configuration
+					0x3B,  // MDMCFG3       Modem Configuration
+					0x73,  // MDMCFG2       Modem Configuration
+					0xA0,  // MDMCFG1       Modem Configuration
+					0xF8,  // MDMCFG0       Modem Configuration
+					0x00,  // DEVIATN       Modem Deviation Setting
+					0x07,  // MCSM2         Main Radio Control State Machine Configuration
+				  	0x0C,  // MCSM1         Main Radio Control State Machine Configuration
+					0x18,  // MCSM0         Main Radio Control State Machine Configuration
+				  	0x1D,  // FOCCFG        Frequency Offset Compensation Configuration
+				  	0x1C,  // BSCFG         Bit Synchronization Configuration
+			    		0xC7,  // AGCCTRL2      AGC Control
+			    		0x40,  // AGCCTRL1      AGC Control
+				  	0xB2,  // AGCCTRL0      AGC Control
+				  	0x02,  // WOREVT1       High Byte Event0 Timeout
+					0x26,  // WOREVT0       Low Byte Event0 Timeout
+					0x09,  // WORCTRL       Wake On Radio Control
+				  	0xB6,  // FREND1        Front End RX Configuration
+					0x17,  // FREND0        Front End TX Configuration
+				  	0xEA,  // FSCAL3        Frequency Synthesizer Calibration
+				  	0x0A,  // FSCAL2        Frequency Synthesizer Calibration
+				  	0x00,  // FSCAL1        Frequency Synthesizer Calibration
+				  	0x19,  // FSCAL0        Frequency Synthesizer Calibration
+					0x41,  // RCCTRL1       RC Oscillator Configuration
+					0x00,  // RCCTRL0       RC Oscillator Configuration
+					0x59,  // FSTEST        Frequency Synthesizer Calibration Control,
+					0x7F,  // PTEST         Production Test
+					0x3F,  // AGCTEST       AGC Test
+					0x81,  // TEST2         Various Test Settings
+					0x3F,  // TEST1         Various Test Settings
+					0x0B   // TEST0         Various Test Settings
+				};
 
 static uint8_t cc1100_OOK_4_8_kb[CFG_REGISTER] = {
 					0x06,  // IOCFG2        GDO2 Output Pin Configuration
-				  0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+				  	0x2E,  // IOCFG1        GDO1 Output Pin Configuration
 					0x06,  // IOCFG0        GDO0 Output Pin Configuration
 					0x47,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
 					0xD3,  // SYNC1         Sync Word, High Byte
@@ -65,10 +284,10 @@ static uint8_t cc1100_OOK_4_8_kb[CFG_REGISTER] = {
 					0x00,  // ADDR          Device Address
 					0x00,  // CHANNR        Channel Number
 					0x06,  // FSCTRL1       Frequency Synthesizer Control
-				  0x00,  // FSCTRL0       Frequency Synthesizer Control
+				  	0x00,  // FSCTRL0       Frequency Synthesizer Control
 					0x21,  // FREQ2         Frequency Control Word, High Byte
 					0x65,  // FREQ1         Frequency Control Word, Middle Byte
-				  0x6A,  // FREQ0         Frequency Control Word, Low Byte
+				  	0x6A,  // FREQ0         Frequency Control Word, Low Byte
 					0x87,  // MDMCFG4       Modem Configuration
 					0x83,  // MDMCFG3       Modem Configuration
 					0x3B,  // MDMCFG2       Modem Configuration
@@ -95,14 +314,14 @@ static uint8_t cc1100_OOK_4_8_kb[CFG_REGISTER] = {
 					0x41,  // RCCTRL1       RC Oscillator Configuration
 					0x00,  // RCCTRL0       RC Oscillator Configuration
 					0x59,  // FSTEST        Frequency Synthesizer Calibration Control
-				  0x7F,  // PTEST         Production Test
+				  	0x7F,  // PTEST         Production Test
 					0x3F,  // AGCTEST       AGC Test
 					0x81,  // TEST2         Various Test Settings
 					0x35,  // TEST1         Various Test Settings
 					0x09,  // TEST0         Various Test Settings
 				};
 
-
+                           //Patable index: -30  -20- -15  -10   0    5    7    10 dBm
 static uint8_t patable_power_315[8] = {0x17,0x1D,0x26,0x69,0x51,0x86,0xCC,0xC3};
 static uint8_t patable_power_433[8] = {0x6C,0x1C,0x06,0x3A,0x51,0x85,0xC8,0xC0};
 static uint8_t patable_power_868[8] = {0x03,0x17,0x1D,0x26,0x50,0x86,0xCD,0xC0};
