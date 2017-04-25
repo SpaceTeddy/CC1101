0d2469e1d6877ab5a2acd9aed38a4f45380c2ed6
SpaceTeddy <spaceteddy1@gmx.net>
2017-04-25 20:55:46 +0200

    Update TX_Demo.cpp

--- examples/raspi/TX_Demo.cpp
+++ examples/raspi/TX_Demo.cpp
@@ -166,25 +166,27 @@ int main(int argc, char *argv[]) {
 	//------------------------- Main Loop ------------------------
 	for (;;)
 	{
-		delay(1);                            //delay to reduce system load
+		delay(1);                                           //delay to reduce system load
 
-		if (millis() - prev_millis_1s_timer >= interval) // one second update timer
+		if (millis() - prev_millis_1s_timer >= interval)    // one second update timer
 		{
-    			Rx_addr = rx_demo_addr;                                      //receiver address
+    			Rx_addr = rx_demo_addr;                     //receiver address
 
-    			uint32_t time_stamp = millis();                              //generate time stamp
+    			uint32_t time_stamp = millis();             //generate time stamp
 
-    			Tx_fifo[3] = (uint8_t)(time_stamp >> 24);                    //split 32-Bit timestamp to 4 byte array
+    			Tx_fifo[3] = (uint8_t)(time_stamp >> 24);   //split 32-Bit timestamp to 4 byte array
     			Tx_fifo[4] = (uint8_t)(time_stamp >> 16);
     			Tx_fifo[5] = (uint8_t)(time_stamp >> 8);
     			Tx_fifo[6] = (uint8_t)(time_stamp);
 
-    			Pktlen = 0x07;                                               //set packet len to 0x13
+    			Pktlen = 0x07;                              //set packet len to 0x13
 
-    			cc1100.sent_packet(My_addr, Rx_addr, Tx_fifo, Pktlen, tx_retries);    //sents package over air. ACK is received via GPIO polling
-
-    			printf("tx_time: %ums \r\n", millis());
-			
+    			uint8_t res = cc1100.sent_packet(My_addr, Rx_addr, Tx_fifo, Pktlen, tx_retries);
+    			
+    			if( res == 1)    //sents package over air. ACK is received via GPIO polling
+    			{
+    				printf("transmitted tx_timestamp: %ums \r\n\r\n", millis());
+				}
 			prev_millis_1s_timer = millis();
   		}
 
