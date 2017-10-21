03b007caeaed1a529d8e8f6f400ed01e9c864171
SpaceTeddy <spaceteddy1@gmx.net>
2017-10-21 14:46:17 +0200

    Update Rx_demo.ino
    - change to EnableInterrupt library (former PinChangeInt)
    - added Arduino sleep mode in demo

--- examples/Arduino/Rx_demo.ino
+++ examples/Arduino/Rx_demo.ino
@@ -1,17 +1,19 @@
 /*-----------------------------------------------------------------------------
 '                     RX_DEMO
 '                     -------
-'  - needs PinChangeInt library
+'  - needs EnableInterrupt (former PinChangeInt) library
 '  - *.eep file must flashed to Arduino first -> use eeprom Tool
 '  - put cc1100_arduino.h in your sketch folder. dont't install it as library 
 '    for this example.
 '  
 '  
 '-----------------------------------------------------------------------------*/
-#include "cc1100_arduino.h"
-#include <PinChangeInt.h>
+#include <avr/sleep.h>
+#include <cc1100.h>
+#include <EnableInterrupt.h>
 
 //---------------------------=[Global variables]=----------------------------
+volatile int sleep_enable = 0;
 uint32_t rf_timecode = 0;
 uint32_t rf_timecode_backup = 0;
 //--------------------------[Global Task variables]--------------------------
@@ -31,26 +33,29 @@ CC1100 cc1100;
 void setup() 
 {
   // init serial Port for debugging
-  Serial.begin(38400);Serial.println();
+  Serial.begin(115200);Serial.println();
   
   // init CC1101 RF-module and get My_address from EEPROM
   cc1100.begin(My_addr);                   //inits RF module with main default settings
   
   cc1100.sidle();                          //set to ILDE first
+  
   cc1100.set_mode(0x04);                   //set modulation mode 1 = GFSK_1_2_kb; 2 = GFSK_38_4_kb; 3 = GFSK_100_kb; 4 = MSK_250_kb; 5 = MSK_500_kb; 6 = OOK_4_8_kb
   cc1100.set_ISM(0x02);                    //set ISM Band 1=315MHz; 2=433MHz; 3=868MHz; 4=915MHz
   cc1100.set_channel(0x01);                //set channel
   cc1100.set_output_power_level(0);        //set PA level in dbm
   cc1100.set_myaddr(0x03);                 //set my own address
   
-  cc1100.spi_write_register(IOCFG2, 0x06); //set module in sync mode detection mode
-  
+  //cc1100.spi_write_register(IOCFG0, 0x24); //set module in sync mode detection mode
+  cc1100.spi_write_register(IOCFG2,0x06);
+
   cc1100.show_main_settings();             //shows setting debug messages to UART    
   cc1100.show_register_settings();         //shows current CC1101 register values
+   
   cc1100.receive();                        //set to RECEIVE mode
-
+  
   // init interrrupt function for available packet
-  attachPinChangeInterrupt(GDO2, rf_available_int, HIGH);
+  enableInterrupt(GDO2, rf_available_int, RISING); 
   
   Serial.println(F("CC1101 RX Demo"));   //welcome message
 }
@@ -64,27 +69,55 @@ void loop()
                   ((uint32_t)Rx_fifo[4] << 16) + 
                   ((uint16_t)Rx_fifo[5] <<  8) + 
                              Rx_fifo[6];
-    Serial.print(F("TX_Timecode: "));Serial.print(rf_timecode);Serial.println(F("ms"));
+    Serial.print(F("TX_Timecode: "));Serial.print(rf_timecode);Serial.println(F("ms\n"));
 
     rf_timecode_backup = millis();
+    
     cc1101_packet_available = FALSE;
   }
-
+  
+  //set Arduino in sleep mode if no package is received within 5 sec.
+  if(millis() - rf_timecode_backup > 5000){
+    rf_timecode_backup = millis();
+    
+    sleep_enable = 1;
+    Serial.println(F("...sleep MCU"));   //welcome message
+    delay(100);
+    set_sleep_mode(SLEEP_MODE_STANDBY);
+    cli();
+    sleep_enable();
+    sei();
+    sleep_cpu();
+    sleep_disable();
+    
+    enableInterrupt(GDO2, rf_available_int, RISING); 
+  }
+  
 }
 //--------------------------[end loop]----------------------------
 
 //---------------------[ check incomming RF packet ]-----------------------
 void rf_available_int(void) 
 {
-  detachPinChangeInterrupt(GDO2);
-  sei();
+  disableInterrupt(GDO2);
+  sei();                                                            //for millis()
   uint32_t time_stamp = millis();                                   //generate time stamp
-    
-  if(cc1100.packet_available() == TRUE){
-    cc1100.get_payload(Rx_fifo, pktlen, rx_addr, sender, rssi_dbm, lqi); //stores the payload data to Rx_fifo
-    cc1101_packet_available = TRUE;                                      //set flag that an package is in RX buffer
+  
+  sleep_disable();
+  sleep_enable = 0;
+
+  if(cc1100.packet_available() == TRUE)
+  {
+    if(cc1100.get_payload(Rx_fifo, pktlen, rx_addr, sender, rssi_dbm, lqi) == TRUE) //stores the payload data to Rx_fifo
+    {
+        cc1101_packet_available = TRUE;                                //set flag that a package is in RX buffer
+        Serial.print(F("rx_time: "));Serial.print(millis()-time_stamp);Serial.println(F("ms"));
+    }
+    else
+    {
+        cc1101_packet_available = FALSE;                               //set flag that an package is corrupted
+    }
   }
-  Serial.print(F("rx_time: "));Serial.print(millis()-time_stamp);Serial.println(F("ms"));
-    
-  attachPinChangeInterrupt(GDO2, rf_available_int, HIGH);
+  
+  enableInterrupt(GDO2, rf_available_int, RISING); 
 }
