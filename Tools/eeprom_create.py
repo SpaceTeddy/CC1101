844f4b41643cf1e251cdde54e4b3fef854c007af
Roman Hoog Antink <rha@disconnect.ch>
2018-10-19 11:47:54 +0200

    fix typo in eeprom_create.py

--- Tools/eeprom_create.py
+++ Tools/eeprom_create.py
@@ -29,7 +29,7 @@ txt = open(filename)
 #print ("Here's your file %r:") % filename
 txt =  txt.read()
 file_len = len(txt)
-print("File lenght:" + str(file_len))
+print("File length:" + str(file_len))
 
 print(txt)
 
@@ -44,26 +44,26 @@ len = len(start)
 loop = 1
 txt_neu = ""
 total_bytes = 0
-max_lenght = 0
+max_length = 0
 f.write("Import file: " + filename + "\n")
 f.write("\n")
 for x in start:
     if(txt[x+7] == '0' and txt[x+8] == '1'):
         print ("EOF!")
         print ("Output array:")
-    lenght = txt[x+1] + txt[x+2]
-    lenght = int(lenght, 16)
-    if(lenght > max_lenght):
-        max_lenght = lenght
-    total_bytes += lenght
-    lenght = (lenght * 2) - 1
+    length = txt[x+1] + txt[x+2]
+    length = int(length, 16)
+    if(length > max_length):
+        max_length = length
+    total_bytes += length
+    length = (length * 2) - 1
     count = 0
     loop += + 1
-    while (count <= lenght):
+    while (count <= length):
         txt_neu += "0x" + txt[x+9+count]
         count = count + 1
         txt_neu += txt[x+9+count]
-        if(loop == len and count == lenght):
+        if(loop == len and count == length):
             txt_neu += "};"
         else:
             txt_neu += ","
@@ -73,16 +73,16 @@ for x in start:
 print ("Total Bytes: " + str(total_bytes))
 #print ("#define EEPROM_LEN " + str(total_bytes))
 print ("uint8_t eeprom_cc1101[FULL_EEPROM_LEN] = {")
-if(max_lenght == 16):
+if(max_length == 16):
     print("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F")
-if(max_lenght == 32):
+if(max_length == 32):
     print("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F   10   11   12   13   14   15   16   17   18   19   1A   1B   1C   1D   1E   1F")
 print (txt_neu)
 #f.write("#define EEPROM_LEN " + str(total_bytes) + "\n")
 f.write("uint8_t eeprom_cc1101[FULL_EEPROM_LEN] = { \n")
-if(max_lenght == 16):
+if(max_length == 16):
     f.write("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F" + "\n")
-if(max_lenght == 32):
+if(max_length == 32):
     f.write("//00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F   10   11   12   13   14   15   16   17   18   19   1A   1B   1C   1D   1E   1F" + "\n")
 f.write(txt_neu)
 f.close()
